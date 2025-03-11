de9040cd51318eb4ac4b411f37afc488b24670d5
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2025-03-11 23:42:13 +0900

    user controller) 회원가입 api

--- book-shop/routes/users.js
+++ book-shop/routes/users.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const conn = require('../db');
+const { Result } = require('express-validator');
+
+const {StatusCodes} = require('http-status-codes');
+
 router.use(express.json());
 
+const {join} = require('../controller/UserController');
+
+
 //회원가입
-router.post('/join', (req, res) => {
-    res.json("join");
-});
+router.post('/join', join);
+
 //로그인
-router.post('/login')
+router.post('/login');
+
 // 비밀번호 초기화 요청
-router.post('/reset')
+router.post('/reset');
+
 //비밀번화 초기화
-router.put('/reset')
+router.put('/reset');
 
 
 module.exports = router
